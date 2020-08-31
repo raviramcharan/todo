57eb4da9a3b73aabe189dbc3db73ef047313cac1
Ravi Ramcharan <rsramcharan@gmail.com>
2020-08-31 12:18:09 +0200

    initial commit

---
+++ assets/js/logic.js
@@ -0,0 +1,24 @@
+//Click event voor afstrepen todo
+$("ul").on("click", "li", function() {
+	$(this).toggleClass("completed");
+});
+
+//Click event om todo te deleten
+$("ul").on("click", "span", function(event) {
+	$(this).parent().fadeOut(500, function() {
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event) {
+	if(event.which === 13) {
+		var todo = $(this).val();
+		$(this).val("");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todo + "</li>")
+	}
+});
+
+$(".fa-plus").click(function() {
+	$("input[type='text']").fadeToggle();
+});
